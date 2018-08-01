62121e7406035d6785cc4b2c3cd2599927f5d0f8
ducminh.tran <ducminh.tran@ved.com.vn>
2018-07-31 18:57:52 -0700

    sql

--- entrytask/deploy/mysql/create_table.sql
+++ entrytask/deploy/mysql/create_table.sql
@@ -6,7 +6,7 @@ CREATE TABLE user_tab (
 	user_name varchar(255) NOT NULL,
     password varchar(255) NOT NULL,
     address varchar(255),
-    create_time datetime,
+    create_time int,
     full_name varchar(255),
     is_active boolean,
     user_type varchar(255),
@@ -16,12 +16,13 @@ CREATE TABLE user_tab (
 	create_by varchar(255),
     PRIMARY KEY (id)
 ) ENGINE=InnoDB;
+CREATE INDEX idx_user_name ON user_tab (user_name);
 
 CREATE TABLE event_tab (
 	id int NOT NULL AUTO_INCREMENT,
     create_by int NOT NULL,
-	create_time datetime,
-    event_time datetime,
+	create_time int,
+    event_time int,
     location varchar(255),
     title varchar(255),
     description VARCHAR(2048),
@@ -35,7 +36,7 @@ CREATE TABLE participant_tab (
 	id int NOT NULL AUTO_INCREMENT,
 	user_id int NOT NULL,
     event_id int NOT NULL,
-    create_time datetime,
+    create_time int,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES user_tab(id),
     FOREIGN KEY (event_id) REFERENCES event_tab(id)
@@ -45,7 +46,7 @@ CREATE INDEX idx_user_id_create_time ON participant_tab (user_id, create_time);
 
 CREATE TABLE comment_tab (
 	id int NOT NULL AUTO_INCREMENT,
-    create_time datetime,
+    create_time int,
 	user_id int NOT NULL,
     event_id int NOT NULL,
     content varchar(512),
@@ -58,7 +59,7 @@ CREATE INDEX idx_user_id_create_time ON comment_tab (user_id, create_time);
 
 CREATE TABLE like_tab (
 	id int NOT NULL AUTO_INCREMENT,
-    create_time datetime,
+    create_time int,
 	user_id int NOT NULL,
     event_id int NOT NULL,
     type varchar(255),
@@ -72,7 +73,7 @@ CREATE INDEX idx_user_id_create_time ON like_tab (user_id, create_time);
 CREATE TABLE category_tab (
 	id int NOT NULL AUTO_INCREMENT,
     category_name varchar(255),
-    create_time datetime,
+    create_time int,
     description varchar(255),
     category_photo varchar(255),
     PRIMARY KEY (id)
@@ -83,7 +84,6 @@ CREATE TABLE event_category_tab (
 	id int NOT NULL AUTO_INCREMENT,
     event_id int NOT NULL,
     category_id int NOT NULL,
-    create_time datetime,
     PRIMARY KEY (id),
     FOREIGN KEY (event_id) REFERENCES event_tab(id),
     FOREIGN KEY (category_id) REFERENCES category_tab(id)
@@ -93,7 +93,7 @@ CREATE INDEX idx_category_id_event_id ON event_category_tab(category_id, event_i
 CREATE TABLE tag_tab (
 	id int NOT NULL AUTO_INCREMENT,
     tag_name varchar(255),
-    create_time datetime,
+    create_time int,
     PRIMARY KEY (id)
 ) ENGINE=InnoDB;
 CREATE INDEX idx_tag_name ON tag_tab(tag_name);
@@ -102,8 +102,6 @@ CREATE TABLE event_tag_tab (
 	id int NOT NULL AUTO_INCREMENT,
     event_id int NOT NULL,
     tag_id int NOT NULL,
-    create_by int NOT NULL,
-    create_time datetime,
     PRIMARY KEY (id),
     FOREIGN KEY (event_id) REFERENCES event_tab(id),
     FOREIGN KEY (tag_id) REFERENCES tag_tab(id)
@@ -114,19 +112,9 @@ CREATE TABLE photo_tab (
 	id int NOT NULL AUTO_INCREMENT,
     photo_url varchar(255),
     photo_content varchar(255),
-    creat_time datetime,
+    creat_time int,
     event_id int NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (event_id) REFERENCES event_tab(id)
 ) ENGINE=InnoDB;
 CREATE INDEX idx_event_id_create_time ON photo_tab(event_id, creat_time);
-
-CREATE TABLE description_tab (
-	id int NOT NULL AUTO_INCREMENT,
-    description varchar(512),
-    creat_time datetime,
-    event_id int NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (event_id) REFERENCES event_tab(id)
-) ENGINE=InnoDB;
-CREATE INDEX idx_event_id_create_time ON description_tab(event_id, creat_time);
